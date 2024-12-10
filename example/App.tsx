4e4996343690e2b0b33304c93a769059c1d5e948
LLmoskk <1398145450@qq.com>
2024-12-10 11:25:55 +0800

    fix: css build and example

---
+++ example/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TimePeriodSelector } from '../src/index';
+
+const App = () => {
+  return (
+    <TimePeriodSelector className='m-auto mt-52' title='Time period selector' />
+  );
+};
+
+export default App;
